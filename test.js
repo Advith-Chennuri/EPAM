5456ca419f1b1fcbc5a3b615a7a9ef0796c8e860
Advith-Chennuri <advith.chennuri@gmail.com>
2025-02-13 15:05:05 +0530

    Up to

---
+++ test.js
@@ -0,0 +1,12 @@
+alert("This is an alert message");
+prompt("Name:")
+confirm("confirm");
+var man=confirm("Are you a man?")
+if(man){
+    let name=prompt("Name");
+    document.write("Welcome "+name);
+}
+else{
+    let name=prompt("Name");
+    document.write("Welcome "+name);
+}
